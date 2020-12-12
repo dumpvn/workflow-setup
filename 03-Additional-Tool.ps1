f6a218c76333e6dd85a43266734908184da2acf4
Phuc Nguyen <phuc.vinh@outlook.com>
2020-12-13 00:44:12 +0700

    node, python

--- 03-Additional-Tool.ps1
+++ 03-Additional-Tool.ps1
@@ -5,6 +5,8 @@ $ToolList = @{
     studio64 = 'https://developer.android.com/studio'
     javac = 'https://www.oracle.com/java/technologies/javase/javase-jdk8-downloads.html'
     Skype = 'https://www.skype.com/en/get-skype/download-skype-for-desktop/'
+    node = 'https://nodejs.org/en/download/'
+    python = 'https://www.python.org/downloads/'
 }
 
 $ToolList.Keys | ForEach-Object {
